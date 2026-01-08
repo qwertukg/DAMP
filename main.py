46fbb0f26999c8b650cb5f1e6b4bdfec5be71044
daniil <daniil.rakhmatulin@gmail.com>
2026-01-08 15:02:33 +0500

    fixes mnist

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import numpy as np
 from MnistSobelAngleMap import MnistSobelAngleMap
 from torchvision.datasets import MNIST
 from torchvision import transforms
+from collections import defaultdict
+
 
 
 def main() -> None:
@@ -35,30 +37,30 @@ def main() -> None:
         ]),
     )
 
-   
-
-    # 1) грузим MNIST (скачает в ./data если нет)
+    codes = defaultdict(list)
     ds = MNIST(root="./data", train=True, download=True, transform=transforms.ToTensor())
-    img_tensor, label = ds[0]        # img_tensor: [1,28,28], float32 0..1
-    img = img_tensor.squeeze(0).numpy()  # -> (28,28) numpy
-
-    # 3) прогоняем через твой extractor
     extractor = MnistSobelAngleMap(angle_in_degrees=True, grad_threshold=0.05)
-    digitValues = extractor.extract(img, label)
+    for i in range(100):
+        img_tensor, label = ds[i]
+        img = img_tensor.squeeze(0).numpy()
 
-    print("label:", label)
-    print("кол-во квадратов:", len(digitValues[label]))
-    print("первые 10:", digitValues[label][:10])
+        digitValues = extractor.extract(img, label)
 
-    for (a, x, y) in digitValues[label]:
-        print(f"{a}\t{x}\t{y}")
-        values, code = encoder.encode(
-            float(a), 
-            float(x), 
-            float(y)
-        )
-        print(f"{values} -> {code}")
-        show(encoder, values, code)
+        print("label:", label)
+        print("кол-во квадратов:", len(digitValues[label]))
+        print("первые 10:", digitValues[label][:10])
+
+        for (a, x, y) in digitValues[label]:
+            print(f"{label} -> \t{a}\t{x}\t{y}")
+            values, code = encoder.encode(
+                float(a), 
+                float(x), 
+                float(y)
+            )
+            print(f"{values} -> {code}")
+            show(encoder, values, code)
+            codes[label].extend(code)
+
 
 
 
