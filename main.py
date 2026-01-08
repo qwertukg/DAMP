569e1d37dd5657c2c5bfbf95b51f876424d6f22c
daniil <daniil.rakhmatulin@gmail.com>
2026-01-08 14:15:37 +0500

    fixes for sobel - mnist

--- main.py
+++ main.py
@@ -2,30 +2,39 @@ from damp_encoder import Encoder, ClosedDimension, OpenedDimension, Detectors
 from visualize_detectors import show, wait_for_close
 
 def main() -> None:
+    
     encoder = Encoder(
-        ClosedDimension((0.0, 179.0), [
+        # Angle
+        ClosedDimension((0.0, 360.0), [
+            Detectors(360, 0.4),
             Detectors(180, 0.4),
             Detectors(90, 0.4),
             Detectors(45, 0.4),
-        ]),
-        OpenedDimension((0.0, 249.0), [
-            Detectors(250, 0.4),
-            Detectors(50, 0.4),
+            Detectors(30, 0.4),
             Detectors(10, 0.4),
-            Detectors(2, 0.4),
+            Detectors(5, 0.4),
         ]),
-        OpenedDimension((0.0, 249.0), [
-            Detectors(250, 0.4),
-            Detectors(50, 0.4),
-            Detectors(10, 0.4),
+        # X
+        OpenedDimension((0, 6), [
+            Detectors(7, 0.4),
+            Detectors(4, 0.4),
             Detectors(2, 0.4),
+            Detectors(1, 0.4),
+        ]),
+        # Y
+        OpenedDimension((0, 6), [
+            Detectors(7, 0.4),
+            Detectors(4, 0.4),
+            Detectors(2, 0.4),
+            Detectors(1, 0.4),
         ]),
     )
 
-    for angle in range(180):
-        for x in range(249):
-            for y in range(249):
+    for angle in range(360):
+        for x in range(28):
+            for y in range(28):
                 values, code = encoder.encode(float(angle), float(x), float(y))
+                print(f"{values} -> {code}")
                 show(encoder, values, code)
     
     wait_for_close()
